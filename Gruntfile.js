62aaa0712f6e4fa7227e31965b5947bd7dda6eab
mjuniper <mjuniper@gmail.com>
2014-04-30 21:14:58 -0600

    fix resume.js location issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -254,7 +254,8 @@ module.exports = function (grunt) {
                         '*.{ico,png,txt}',
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
-                        'fonts/{,*/}*.*'
+                        'fonts/{,*/}*.*',
+                        'data/resume.js'
                     ]
                 }]
             },
